4dfdbc241eb3a3211f3b005d0e6087c321186bef
aminamin13 <amin.hassan.1324@gmail.com>
2023-08-11 15:10:13 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const cors = require('cors')
+const xls = require('xlsx')
+
+const app = express()
+
+app.use(cors())
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/services',(req,res)=>{
+
+    let wb = xls.readFile('data.xlsx');
+    let ws = wb.Sheets['Sheet1'];
+    let data = xls.utils.sheet_to_json(ws)
+    res.send(data);
+
+
+
+
+//     let data = [
+        
+//         {name:'Location', description:'desc1',icon:'map'},
+//         { name:'Alexa', description:'desc2',icon:'alexa'},
+//         { name:'Briefcase', description:'desc3',icon:'briefcase'},
+//     ]
+// res.send(data)
+
+
+})
+
+app.listen(3000)
